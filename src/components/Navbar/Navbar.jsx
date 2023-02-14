c3a0441fe182f592b074b99a3746b2e4461602cb
Amiraliansaripour <amiraliansaripor1383@gmail.com>
2023-02-14 15:37:10 +0330

    Responsive Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 const Navbar = () => {
+    const navRef = useRef()
+    const resMenu =() =>{
+        navRef.current.classList.toggle("active")
+    }
     return (
         <div className="header-list">
             {/* logo */}
@@ -9,19 +13,22 @@ const Navbar = () => {
                 <h1>Perfect</h1>
             </div>
             <div className="links">
-                <ul>
+                <ul ref={navRef}>
                     <li><Link to="/">صفحه اصلی</Link></li>
                     <li><Link to="/team">تیم پرفکت</Link></li>
                     <li><Link to="/about">درباره ما</Link></li>
                     <li><Link to="/contact">تماس باما</Link></li>
+                    <li className='close' onClick={resMenu}>
+                        <i className='fa fa-remove'></i>
+                    </li>
                 </ul>
             </div>
-            <div className="btn-active">
+            <div className="btn-active contact-btn">
                 <Link to="/contact">تماس باما</Link>
             </div>
 
-            <div className='hamburger'>
-                <i className='fa fa-navicon'></i>
+            <div className="hamburger" onClick={resMenu}>
+                <i className="fa fa-navicon"></i>
             </div>
         </div>
     )
