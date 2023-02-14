d0555eb7f6a542e9e0def28796310c6cb3f7d211
mhdix <domanloom@gmail.com>
2023-02-14 19:05:59 +0330

    added folders and program aboutUs

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './components/Home/Home'
 import NotFound from './components/NotFound/NotFound'
+import AboutUs from './components/aboutUs/AboutUs';
 
 
 
 const routes = [
     {path:'/' , exact: "true", element:<Home />},
+    {path:'/about' , exact: "true", element:<AboutUs />},
     {path:'*' ,element:<NotFound />},
 ]   
 export default routes
---
+++ src/components/aboutUs/aboutUsHero/AboutUsHero.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './AboutUsHero.css';
+import { Link } from 'react-router-dom';
+const AboutUsHero = () => {
+  return (
+    <>
+      <div className='header about-us-hero'>
+        <div className="header-content p-3">
+          <div className="content">
+            <h1>
+              تیم استارتاپی پرفکت کد
+            </h1>
+            <p>
+              ما در تیم پرفکت کد قصد داریم با بروز ترین امکانات موجود سایت شمارو طراحی کنیم و به کسب و کار شما رونق ببخشیم ، تیم خبره ما هر آنچیزی که شما نیاز داشته باشید رو براتون خلق میکنه فقط کافیه روی دکمه زیر بزنی تا به صورت رایگان مشاوره بگیری.
+            </p>
+            <div className="join-btn">
+              <Link to="/" className="btn-hover">تیم پرفکت </Link>
+              <Link to="/" className="btn-active">مشاوره اختصاصی</Link>
+            </div>
+          </div>
+          <div className="bg-img w-50 z-2">
+          </div>
+        </div>
+      </div>
+    </>
+
+
+  )
+}
+
+export default AboutUsHero
