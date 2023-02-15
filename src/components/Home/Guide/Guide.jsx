03a094cbb80307f66a3c7cbe0734ec63a253075a
Amiraliansaripour <amiraliansaripor1383@gmail.com>
2023-02-15 13:48:43 +0330

    Guide Component layout

--- src/components/Home/Guide/Guide.jsx
+++ src/components/Home/Guide/Guide.jsx
@@ -4,7 +4,17 @@ const Guide = () => {
   return (
     <div className="guide">
         <h2>نحوه ثبت سفارش</h2>
-        
+        <div className="guide-section">
+            <div className="guide-img">
+                <img src="../assets/stylingImage/login.png" alt="" />
+            </div>
+
+            <div className="guide-text">
+               <div className="guide-box">
+                
+               </div>
+            </div>
+        </div>
     </div>
   )
 }
