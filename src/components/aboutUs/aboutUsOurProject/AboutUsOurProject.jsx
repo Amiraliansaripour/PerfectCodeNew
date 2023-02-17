125539fcce36650fe231f70c74da1c3b3d3c54dc
mhdix <domanloom@gmail.com>
2023-02-17 20:35:34 +0330

    fixed responsive

--- src/components/aboutUs/aboutUsOurProject/AboutUsOurProject.jsx
+++ src/components/aboutUs/aboutUsOurProject/AboutUsOurProject.jsx
@@ -5,10 +5,10 @@ const AboutUsOurProject = () => {
     <section className='content our-project d-flex flex-row-reverse justify-content-center align-items-center'>
       <div className='our-project-bg-img'></div>
       <div className=' our-project-desc d-flex flex-column align-item-self-start flex-end'>
-        <h2 className='w-75'>سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</h2>
-        <p className='w-75'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابها .</p>
-        <div className="line"></div>
-        <div className='our-project-number d-flex flex-row text-start align-self-end'>
+        <h2 className='w-100 px-4'>سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</h2>
+        <p className='w-100 px-4'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابها .</p>
+        <div className="line d-lg-none d-block"></div>
+        <div className='our-project-number d-flex flex-row text-start align-self-end px-4'>
           
           <div className='me-5'>
             <h2>400</h2>
