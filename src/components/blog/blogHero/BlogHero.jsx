db4d7c332d87c72a2ce8a8c0fb1e1fad1609310f
mhdix <domanloom@gmail.com>
2023-02-20 00:50:18 +0330

    fixed responsive blogHero

--- src/components/blog/blogHero/BlogHero.jsx
+++ src/components/blog/blogHero/BlogHero.jsx
@@ -3,18 +3,19 @@ import './BlogHero.css'
 const BlogHero = () => {
   return (
     <section className='container content blog d-flex flex-column-reverse flex-lg-row justify-content-start align-items-start'>
-      <div className='col-10 col-xl-5 col-lg-5 mx-lg-auto mini-blog mt-5 mx-auto'>
-        <div className="mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
+      <div className='col-10 col-xl-5 col-lg-5 d-flex flex-row flex-wrap justify-content-evenly flex mx-lg-auto mini-blog mx-auto '>
+        <h1 className='mt-5 mt-lg-0'>تازه ترین اخبار ها</h1>
+        <div className="col-5 col-md-12 mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
           <img src="../assets/img/blog/mini1.png" width={240} className='img-fluid' alt="blog title" />
-          <strong className='px-5 text-center mt-xl-0 mt-4'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
+          <strong className='px-0 px-md-4 text-center mt-xl-0 mt-lg-4 mt-4 mt-md-0'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
         </div>
-        <div className="mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
+        <div className="col-5 col-md-12 mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
           <img src="../assets/img/blog/mini1.png" width={240} className='img-fluid' alt="blog title" />
-          <strong className='px-5 text-center mt-xl-0 mt-4'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
+          <strong className='px-0 px-md-4 text-center mt-xl-0 mt-lg-4 mt-4 mt-md-0'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
         </div>
-        <div className="mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
+        <div className="col-5 col-md-12 mini-blog-item d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center ">
           <img src="../assets/img/blog/mini1.png" width={240} className='img-fluid' alt="blog title" />
-          <strong className='px-5 text-center mt-xl-0 mt-4'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
+          <strong className='px-0 px-md-4 text-center mt-xl-0 mt-lg-4 mt-4 mt-md-0'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</strong>
         </div>
       </div>
       <div className='col-10 col-xl-6 col-lg-6 mx-lg-auto blog-post mx-auto'>
