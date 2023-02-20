ba402a24170c8e2afe7b71a136342478086a3522
mhdix <domanloom@gmail.com>
2023-02-20 18:12:06 +0330

    add ready component and optimize commnet componnet

---
+++ src/components/Home/Comments/raedyComponent/CommentBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CommentBox = ({ img, name, tagName, tagLink, desc }) => {
+    return (
+        <div className="comment-slide">
+            <div className="comment-slide-title mt-3">
+                <div className="comment-profile">
+                    <div className="comment-img">
+                        <img src={img} alt={name} />
+                    </div>
+                    <div className="comment-profile-name">
+                        <p>{name}</p>
+                        <a href={tagLink}>{tagName}</a>
+                    </div>
+                </div>
+                <div className="comment-instagram">
+                    <a href="instagram:">
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="comment-slide-decription">
+                <p>{desc}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CommentBox
