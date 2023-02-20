af9db5fab93c58afe7da98ed7606df7a63862f01
mhdix <domanloom@gmail.com>
2023-02-20 18:54:17 +0330

    add basic pricing component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,13 +3,15 @@ import NotFound from './pages/NotFound/NotFound'
 import AboutUsPage from './pages/AboutUsPage';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
+import PricingPage from './pages/PricingPage';
 
 
 
 const routes = [
-    {path:'/blog' , exact: "true", element:<BlogPage />},
-    {path:'/about' , exact: "true", element:<AboutUsPage />},
-    {path:'/' , exact: "true", element:<HomePage />},
-    {path:'*' ,element:<NotFound />},
-]   
+    { path: '/blog', exact: "true", element: <BlogPage /> },
+    { path: '/about', exact: "true", element: <AboutUsPage /> },
+    { path: '/pricing', exact: "true", element: <PricingPage /> },
+    { path: '/', exact: "true", element: <HomePage /> },
+    { path: '*', element: <NotFound /> },
+]
 export default routes
---
+++ src/components/pricing/readyComponent/PriceBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const PriceBox = ({planName , bgColor , btnColor , price , inCluded}) => {
+    return (
+        <div className='price-box d-flex  col-11 col-md-4 mx-md-2' style={{backgroundColor: bgColor}}>
+            <p>fgdg</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    )
+}
+
+export default PriceBox
