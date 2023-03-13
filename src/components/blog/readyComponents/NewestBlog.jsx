91cf76cbd254b9e523ca48156063870c7946b7e6
mhdix <domanloom@gmail.com>
2023-03-13 23:09:53 +0330

    added sitemap

--- src/components/blog/readyComponents/NewestBlog.jsx
+++ src/components/blog/readyComponents/NewestBlog.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NewestBlog = ({ img, alt, title, id }) => {
     return (
         <div className="mini-blog-item col-5 col-md-12 d-flex flex-column flex-md-row-reverse flex-sm-column flex-xl-row-reverse flex-lg-column justify-content-center mt-5 align-items-center " key={id}>
-            <img src={img} width={240} className='img-fluid' alt={alt} />
+            <img src={img} width={240} height={540} className='img-fluid' alt={alt} />
             <strong className='px-0 px-md-4 text-center mt-xl-0 mt-lg-4 mt-2 mt-md-0'>
 
                 <Link to={`/blogpost/${id}`}>
