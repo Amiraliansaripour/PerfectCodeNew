463208981aa0428ecd7695171d2f99c198aee542
mhdix <domanloom@gmail.com>
2024-05-09 02:27:43 -0700

    fixed about us page responsive

--- src/components/aboutUs/ourTeam/readyComponent/OurTeamBox.jsx
+++ src/components/aboutUs/ourTeam/readyComponent/OurTeamBox.jsx
@@ -3,36 +3,57 @@ import Tooltip from 'rc-tooltip';
 
 const OurTeamBox = ({ name, identity, img, telegram, phone, github }) => {
     return (
-        <div className='box box-1 p-5 d-flex flex-column gap-2'>
-            <img src={img} alt={`تیم پرفکت کد ${name}`} width='170' height='170' />
-            <h3 className='mt-3'>{name}</h3>
-            <p>{identity}</p>
-            <div className='ourTeamsocial d-flex align-items-center justify-content-evenly'>
-
-                <Tooltip placement="bottom" trigger={['hover']} overlay={<span>پیام در تلگرام</span>}>
-                    <a href={`https://telegram.me/${telegram}`} target='_blank' className="btn-floating m-1" role="button">
-                        <div className='ourTeamsocialItem d-flex justify-content-center align-items-center'>
-                            <i className="fa-brands fa-telegram"></i>
-                        </div>
-                    </a>
-                </Tooltip>
-                <Tooltip placement="bottom" trigger={['hover']} overlay={<span>برقراری تماس</span>}>
-                    <a href={`tel:${phone}`} className="btn-floating m-1" role="button">
-                        <div className='ourTeamsocialItem d-flex justify-content-center align-items-center'>
-                            <i className="fa fa-phone"></i>
-                        </div>
-                    </a>
-                </Tooltip>
-                <Tooltip placement="bottom" trigger={['hover']} overlay={<span>مشاهده گیت هاب</span>}>
-                <a href={`https://github.com/${github}`} target='_blank' className="btn-floating m-1" role="button">
-                    <div className='ourTeamsocialItem d-flex justify-content-center align-items-center'>
-                        <i className="fa-brands fa-github"></i>
-                    </div>
-                </a>
-                </Tooltip>
-            </div>
+      <div className="box box-1 p-5 d-flex flex-column gap-2">
+        <img src={img} alt={`تیم پرفکت کد ${name}`} width={190} height={190}/>
+        <h3 className="mt-3">{name}</h3>
+        <p>{identity}</p>
+        <div className="ourTeamsocial d-flex align-items-center justify-content-evenly">
+          <Tooltip
+            placement="bottom"
+            trigger={["hover"]}
+            overlay={<span>پیام در تلگرام</span>}
+          >
+            <a
+              href={`https://telegram.me/${telegram}`}
+              target="_blank"
+              className="btn-floating m-1"
+              role="button"
+            >
+              <div className="ourTeamsocialItem d-flex justify-content-center align-items-center">
+                <i className="fa-brands fa-telegram"></i>
+              </div>
+            </a>
+          </Tooltip>
+          <Tooltip
+            placement="bottom"
+            trigger={["hover"]}
+            overlay={<span>برقراری تماس</span>}
+          >
+            <a href={`tel:${phone}`} className="btn-floating m-1" role="button">
+              <div className="ourTeamsocialItem d-flex justify-content-center align-items-center">
+                <i className="fa fa-phone"></i>
+              </div>
+            </a>
+          </Tooltip>
+          <Tooltip
+            placement="bottom"
+            trigger={["hover"]}
+            overlay={<span>مشاهده گیت هاب</span>}
+          >
+            <a
+              href={`https://github.com/${github}`}
+              target="_blank"
+              className="btn-floating m-1"
+              role="button"
+            >
+              <div className="ourTeamsocialItem d-flex justify-content-center align-items-center">
+                <i className="fa-brands fa-github"></i>
+              </div>
+            </a>
+          </Tooltip>
         </div>
-    )
+      </div>
+    );
 }
 
 export default OurTeamBox
